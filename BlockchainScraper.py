2aac285179baa2ae0370c8cdb70e2e3d5c8d7b16
Rehtsecp <rehtseput@gmail.com>
2022-03-02 12:06:28 +0100

    Modified Blockchainscraper.py

--- BlockchainScraper.py
+++ BlockchainScraper.py
@@ -3,6 +3,6 @@ import pandas as pandas
 import requests
 
 url = r"https://www.blockchain.com/btc/unconfirmed-transactions"
-request = requests.get(url)
+request = requests.get(url, "html.parser")
 soup = bs(request.text)
 print(soup.prettify)
